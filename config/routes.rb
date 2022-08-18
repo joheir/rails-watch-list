ba0149c070ce1f8e2332308c527b8f9ab897f2fb
Johanna Bjornsdottir <johannaeir@simnet.is>
2022-08-18 15:41:12 +0200

    backend ready

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
+  resources :lists, only: %I[index show new create] do
+    resources :bookmarks, only: %I[new create ]
+  end
+  resources :bookmarks, only: %I[destroy]
   # Defines the root path route ("/")
-  # root "articles#index"
+  root "lists#index"
 end
