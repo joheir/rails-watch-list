c91c18296e1588013c2ea1e17e093ef79d29fc7e
Johanna Bjornsdottir <johannaeir@simnet.is>
2022-08-19 11:41:55 +0200

    message

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   resources :lists, only: %I[index show new create] do
-    resources :bookmarks, only: %I[new create ]
+    resources :bookmarks, only: %I[new create]
   end
   resources :bookmarks, only: %I[destroy]
   # Defines the root path route ("/")
